fb0a93e0b8c508a7b29a05b2e86202161b80c724
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2025-04-14 21:09:35 +0530

    added

---
+++ frontend/lib/blockchain/contract.ts
@@ -0,0 +1,11 @@
+import { ethers } from "ethers";
+import { getContract } from "./provider";
+
+export const AirQualityRecordABI = [];
+
+export const AirQualityRecordAddress = process.env
+  .NEXT_PUBLIC_CONTRACT_ADDRESS as string;
+
+export function getAirQualityContract() {
+  return getContract(AirQualityRecordAddress, AirQualityRecordABI);
+}
